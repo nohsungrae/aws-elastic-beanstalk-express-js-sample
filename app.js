5d504ecb934e54fab08d797e2154cba4543cfb31
nosungrae <sungrae@mz.co.kr>
2023-03-17 14:18:03 +0900

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Git Test!'));
+app.get('/', (req, res) => res.send('im no manager!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
